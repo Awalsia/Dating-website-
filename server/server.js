8bf18773861b9926632dffc3e0eeb2ae9550b7d6
Stefano Fabiano <108979350+Awalsia@users.noreply.github.com>
2026-06-19 13:12:15 +0200

    Restrict CORS to specific origin and methods

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://dating-website-stefano.netlify.app",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
